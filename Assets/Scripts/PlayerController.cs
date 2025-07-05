d19d6a05e6ec32b29215f2dfe9b54fcc7f47fddb
Hamad Alaslani <hamadsmma@gmail.com>
2025-07-05 09:39:33 +0300

    Add moving obstacles and background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
     private Rigidbody rb;
+    private bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.up * 500);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
